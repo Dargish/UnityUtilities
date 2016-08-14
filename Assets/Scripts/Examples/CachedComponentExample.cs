3716171dedcec475f143fbf93a6930c89c7d4fc2
Dargish <Dargish@gmail.com>
2016-08-14 21:05:57 +0100

    Added CachedComponent Example

---
+++ Assets/Scripts/Examples/CachedComponentExample.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CachedComponentExample : MonoBehaviour
+{
+    public GameObject target;
+
+    CachedComponent<Rigidbody> targetRigidbody;
+
+    void Start()
+    {
+        targetRigidbody = new CachedComponent<Rigidbody>(target);
+    }
+
+    void Update()
+    {
+        if (targetRigidbody.get() != null)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                targetRigidbody.get().AddForce(Vector3.up * 300.0f);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/CachedComponent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CachedComponent<T> where T : Component
+{
+    GameObject gameObject;
+    T component;
+
+    public CachedComponent(GameObject gameObject)
+    {
+        this.gameObject = gameObject;
+    }
+
+    public T get()
+    {
+        if (component == null)
+        {
+            if (gameObject != null)
+            {
+                component = gameObject.GetComponent<T>();
+            }
+        }
+        return component;
+    }
+}
